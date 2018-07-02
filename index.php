4b8d267c2e15ee3ffb1c7eb0513a1f281c56d42e
Joshebube <39905796+Joshebube@users.noreply.github.com>
2018-07-02 09:49:20 -0400

    Add files via upload
    updated home page

--- index.php
+++ index.php
@@ -24,12 +24,12 @@
         <div class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
                 <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="">
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">e-LogBook</a>
+                    <a class="navbar-brand" href="index.php">e-LogBook</a>
                 </div>
                 <div class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
@@ -39,13 +39,11 @@
                         <li>
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown</a>
                             <ul class="dropdown-menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
+                                <li><a href="#">Vision</a></li>
+                                <li><a href="#">Terms</a></li>
+                                <li><a href="#">Security</a></li>
                                 <li class="divider"></li>
-                                <li class="dropdown-header">Nav header</li>
-                                <li><a href="#">Seperated link</a></li>
-                                <li><a href="#">One more seperated link</a></li>
+                                
                             </ul>
                         </li>
                     </ul>
@@ -65,9 +63,11 @@
 
         <div class="jumbotron">
             <div class="container">
-                <h1>Hello, World</h1>
+                <h1>e-LogBook</h1>
 
-                <p>This is a template for a simple marketing or informational website. it includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique</p>
+                <p>This is a platform that aims to bridge the gap between supervisors and interns,it is a better monitoring tool for training programmes,it also enables better continuous and prospective monitoring as compared to conventional log book. It is also able to hold more data.This platform also tracks performance and helps in getting timely feedback from their supervisors. and address other limitations of conventional logbooks.
+
+</p>
 
                 <p><a href="registration_form.php" class="btn btn-primary btn-lg">Register Now &raquo;</a></p>
             </div>
@@ -76,30 +76,51 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <h2>Vision</h2>
+                    <p>1. To provide opportunities of applying their knowledge to real practice thereby making easier the college work.<br/></p>
+ 
+<p>2. To enable students to practice theory been taught at school and provide access to the production equipment not available in the school.<br/></p>
 
-                    <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+<p>3. To provide skillful experience to all students in the school of technologies, polytechnics and universities.<br/></p>
+
+<p>4. To inspire students about their careers<br/></p>
+
+<p>5. To provide more man power to industries.<br/></p>
+
+
+
+<p>Traditionally, a logbook has been in existence since the invention of ships and the need for navigation came into existence. Today’s logbooks have diverse uses especially in schools. Almost all aspects of human endeavor need a form of reference or storage media.</p>
+
+
+                    
                 </div>
                 <div class="col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <h2>Terms</h2>
+                    <p>User Accounts and Organizations have different administrative controls; a human must create your Account; you must be 13 or over; you must provide a valid email address; and you may not have more than one free Account. You alone are responsible for your Account and anything that happens while you are signed in to or using your Account. You are responsible for keeping your Account secure. </p>
+					<p>You agree not to reproduce, duplicate, copy, sell, resell or exploit any portion of the Service, use of the Service, or access to the Service without Administrator's express written permission.</p>
 
-                    <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+                    
                 </div>
                 <div class="col-lg-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-
-                    <p><a class="btn btn-default" href="#">View details &raquo;</a></p>
+                    <h2>Security</h2>
+                    <p>We employ a team of 24/7/365 server specialists to keep our software and its dependencies up to date eliminating potential security vulnerabilities. We employ a wide range of monitoring solutions for preventing and eliminating attacks to the site. </p>
+					
+					  <p>1. Our primary data center operations are regularly audited by independent firms against an ISAE 3000/AT 101 Type 2 Examination standard<br/></p>
+					 <p> 2. we have a fully documented change-management procedures and Secure document-destruction policies for all sensitive information<br/></p>
+					 <p> 3. Systems access logged and tracked for auditing purposes.<br/></p>
+                    
                 </div>
             </div>
 
             <hr>
 
             <footer>
-                <p>&copy; AirPupnMeow.com</p>
-            </footer>
+-                <p>&copy; e-LogBook.com</p>
++            <footer class="page-footer font-small unique-color-dark mt-4">
++                <div class="footer-copyright text-center py-3">
++                    <p>&copy; e-LogBook.com</p>
++                </div>
+             </footer>
         </div>
         <!-- /container -->
 
