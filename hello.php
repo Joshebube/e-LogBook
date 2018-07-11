29220e5098c29b23d83e1e00688adf5cb05989f1
sadiq-dodo <39120468+sadiq-dodo@users.noreply.github.com>
2018-07-11 09:55:36 +0100

    Add files via upload
    reformed thank you page

--- hello.php
+++ hello.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="favicon.ico">
 
-        <title>e-LogBook.com: Just trying to have some fun!</title>
+<?php require_once __DIR__ . "/header.php"; ?>
 
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/main.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]-->
-        <script src="js/html5shiv.js"></script>
-        <script src="js/respond.min.js"></script>
-        <!--[endif] -->
-    </head>
-    <body>
         <h1> Thank you for completing this form, stay safe </h1>
-        <footer>
-                <p>&copy; AirPupnMeow.com</p>
-            </footer>
-        </div>
-        <!-- /container -->
 
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>
-
+<?php require_once __DIR__ . "/footer.php"; ?>
