f6ee903569cc215c313051dee2a1dcd01d719d29
Joshebube <39905796+Joshebube@users.noreply.github.com>
2018-07-16 09:07:18 +0100

    updated index.php

--- index.php
+++ index.php
@@ -1,66 +1,4 @@
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-        <meta name="description" content="">
-        <meta name="author" content="">
-        <link rel="shortcut icon" href="favicon.ico">
-
-        <title>e-LogBook.com: Just trying to have some fun!</title>
-
-        <link href="css/bootstrap.css" rel="stylesheet">
-        <link href="css/main.css" rel="stylesheet">
-
-        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-        <!--[if lt IE 9]-->
-        <script src="js/html5shiv.js"></script>
-        <script src="js/respond.min.js"></script>
-        <!--[endif] -->
-    </head>
-
-    <body>
-    
-        <div class="navbar navbar-inverse navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.php">e-LogBook</a>
-                </div>
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#contact">Contact</a></li>
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Vision</a></li>
-                                <li><a href="#">Terms</a></li>
-                                <li><a href="#">Security</a></li>
-                                <li class="divider"></li>
-                                
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-right">
-                        <div class="form-group">
-                            <input type="text" placeholder="Email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" placeholder="Password" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-success">Sign in</button>
-                    </form>
-                </div>
-                <!--/ .navbar-collapse -->
-            </div>
-        </div>
+<?php require_once __DIR__ . '/header.php'; ?>
 
         <div class="jumbotron">
             <div class="container">
@@ -113,20 +51,4 @@
                 </div>
             </div>
 
-            <hr>
-
-            <footer>
--                <p>&copy; e-LogBook.com</p>
-+            <footer class="page-footer font-small unique-color-dark mt-4">
-+                <div class="footer-copyright text-center py-3">
-+                    <p>&copy; e-LogBook.com</p>
-+                </div>
-             </footer>
-        </div>
-        <!-- /container -->
-
-        <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-    </body>
-</html>
-
+           <?php require_once __DIR__ . "/footer.php"; ?>
